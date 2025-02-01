82679aa4fb16e3bce7519c43435aa8d8f74c7b4b
Piotr Durlej <piotr@durlej.net>
2025-02-01 01:18:55 +0100

    remove the console shell

--- main/tusb_msc_main.c
+++ main/tusb_msc_main.c
@@ -15,7 +15,6 @@
 #include <dirent.h>
 #include <stdlib.h>
 #include "sdkconfig.h"
-#include "esp_console.h"
 #include "esp_check.h"
 #include "esp_partition.h"
 #include "driver/gpio.h"
@@ -41,7 +40,6 @@
 #endif
 
 static const char *TAG = "example_main";
-static esp_console_repl_t *repl = NULL;
 
 /* TinyUSB descriptors
    ********************************************************************* */
@@ -116,167 +114,6 @@ static char const *string_desc_arr[] = {
     "Example MSC",                  // 4. MSC
 };
 /*********************************************************************** TinyUSB descriptors*/
-
-#define BASE_PATH "/data" // base path to mount the partition
-
-#define PROMPT_STR CONFIG_IDF_TARGET
-static int console_unmount(int argc, char **argv);
-static int console_read(int argc, char **argv);
-static int console_write(int argc, char **argv);
-static int console_size(int argc, char **argv);
-static int console_status(int argc, char **argv);
-static int console_exit(int argc, char **argv);
-const esp_console_cmd_t cmds[] = {
-    {
-        .command = "read",
-        .help = "read BASE_PATH/README.MD and print its contents",
-        .hint = NULL,
-        .func = &console_read,
-    },
-    {
-        .command = "write",
-        .help = "create file BASE_PATH/README.MD if it does not exist",
-        .hint = NULL,
-        .func = &console_write,
-    },
-    {
-        .command = "size",
-        .help = "show storage size and sector size",
-        .hint = NULL,
-        .func = &console_size,
-    },
-    {
-        .command = "expose",
-        .help = "Expose Storage to Host",
-        .hint = NULL,
-        .func = &console_unmount,
-    },
-    {
-        .command = "status",
-        .help = "Status of storage exposure over USB",
-        .hint = NULL,
-        .func = &console_status,
-    },
-    {
-        .command = "exit",
-        .help = "exit from application",
-        .hint = NULL,
-        .func = &console_exit,
-    }
-};
-
-// mount the partition and show all the files in BASE_PATH
-static void _mount(void)
-{
-    ESP_LOGI(TAG, "Mount storage...");
-    ESP_ERROR_CHECK(tinyusb_msc_storage_mount(BASE_PATH));
-
-    // List all the files in this directory
-    ESP_LOGI(TAG, "\nls command output:");
-    struct dirent *d;
-    DIR *dh = opendir(BASE_PATH);
-    if (!dh) {
-        if (errno == ENOENT) {
-            //If the directory is not found
-            ESP_LOGE(TAG, "Directory doesn't exist %s", BASE_PATH);
-        } else {
-            //If the directory is not readable then throw error and exit
-            ESP_LOGE(TAG, "Unable to read directory %s", BASE_PATH);
-        }
-        return;
-    }
-    //While the next entry is not readable we will print directory files
-    while ((d = readdir(dh)) != NULL) {
-        printf("%s\n", d->d_name);
-    }
-    return;
-}
-
-// unmount storage
-static int console_unmount(int argc, char **argv)
-{
-    if (tinyusb_msc_storage_in_use_by_usb_host()) {
-        ESP_LOGE(TAG, "storage is already exposed");
-        return -1;
-    }
-    ESP_LOGI(TAG, "Unmount storage...");
-    ESP_ERROR_CHECK(tinyusb_msc_storage_unmount());
-    return 0;
-}
-
-// read BASE_PATH/README.MD and print its contents
-static int console_read(int argc, char **argv)
-{
-    if (tinyusb_msc_storage_in_use_by_usb_host()) {
-        ESP_LOGE(TAG, "storage exposed over USB. Application can't read from storage.");
-        return -1;
-    }
-    ESP_LOGD(TAG, "read from storage:");
-    const char *filename = BASE_PATH "/README.MD";
-    FILE *ptr = fopen(filename, "r");
-    if (ptr == NULL) {
-        ESP_LOGE(TAG, "Filename not present - %s", filename);
-        return -1;
-    }
-    char buf[1024];
-    while (fgets(buf, 1000, ptr) != NULL) {
-        printf("%s", buf);
-    }
-    fclose(ptr);
-    return 0;
-}
-
-// create file BASE_PATH/README.MD if it does not exist
-static int console_write(int argc, char **argv)
-{
-    if (tinyusb_msc_storage_in_use_by_usb_host()) {
-        ESP_LOGE(TAG, "storage exposed over USB. Application can't write to storage.");
-        return -1;
-    }
-    ESP_LOGD(TAG, "write to storage:");
-    const char *filename = BASE_PATH "/README.MD";
-    FILE *fd = fopen(filename, "r");
-    if (!fd) {
-        ESP_LOGW(TAG, "README.MD doesn't exist yet, creating");
-        fd = fopen(filename, "w");
-        fprintf(fd, "Mass Storage Devices are one of the most common USB devices. It use Mass Storage Class (MSC) that allow access to their internal data storage.\n");
-        fprintf(fd, "In this example, ESP chip will be recognised by host (PC) as Mass Storage Device.\n");
-        fprintf(fd, "Upon connection to USB host (PC), the example application will initialize the storage module and then the storage will be seen as removable device on PC.\n");
-        fclose(fd);
-    }
-    return 0;
-}
-
-// Show storage size and sector size
-static int console_size(int argc, char **argv)
-{
-    if (tinyusb_msc_storage_in_use_by_usb_host()) {
-        ESP_LOGE(TAG, "storage exposed over USB. Application can't access storage");
-        return -1;
-    }
-    uint32_t sec_count = tinyusb_msc_storage_get_sector_count();
-    uint32_t sec_size = tinyusb_msc_storage_get_sector_size();
-    printf("Storage Capacity %lluMB\n", ((uint64_t) sec_count) * sec_size / (1024 * 1024));
-    return 0;
-}
-
-// exit from application
-static int console_status(int argc, char **argv)
-{
-    printf("storage exposed over USB: %s\n", tinyusb_msc_storage_in_use_by_usb_host() ? "Yes" : "No");
-    return 0;
-}
-
-// exit from application
-static int console_exit(int argc, char **argv)
-{
-    tinyusb_msc_unregister_callback(TINYUSB_MSC_EVENT_MOUNT_CHANGED);
-    tinyusb_msc_storage_deinit();
-    tinyusb_driver_uninstall();
-    printf("Application Exit\n");
-    repl->del(repl);
-    return 0;
-}
 
 // callback that is delivered when storage is mounted/unmounted by application.
 static void storage_mount_changed_cb(tinyusb_msc_event_t *event)
@@ -426,9 +263,6 @@ void app_main(void)
     ESP_ERROR_CHECK(tinyusb_msc_register_callback(TINYUSB_MSC_EVENT_MOUNT_CHANGED, storage_mount_changed_cb)); /* Other way to register the callback i.e. registering using separate API. If the callback had been already registered, it will be overwritten. */
 #endif  // CONFIG_EXAMPLE_STORAGE_MEDIA_SPIFLASH
 
-    //mounted in the app by default
-    // _mount();
-
     ESP_LOGI(TAG, "USB MSC initialization");
     const tinyusb_config_t tusb_cfg = {
         .device_descriptor = &descriptor_config,
@@ -445,31 +279,4 @@ void app_main(void)
     };
     ESP_ERROR_CHECK(tinyusb_driver_install(&tusb_cfg));
     ESP_LOGI(TAG, "USB MSC initialization DONE");
-
-    esp_console_repl_config_t repl_config = ESP_CONSOLE_REPL_CONFIG_DEFAULT();
-    /* Prompt to be printed before each line.
-     * This can be customized, made dynamic, etc.
-     */
-    repl_config.prompt = PROMPT_STR ">";
-    repl_config.max_cmdline_length = 64;
-
-    // Init console based on menuconfig settings
-#if defined(CONFIG_ESP_CONSOLE_UART_DEFAULT) || defined(CONFIG_ESP_CONSOLE_UART_CUSTOM)
-    esp_console_dev_uart_config_t hw_config = ESP_CONSOLE_DEV_UART_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_console_new_repl_uart(&hw_config, &repl_config, &repl));
-
-    // USJ console can be set only on esp32p4, having separate USB PHYs for USB_OTG and USJ
-#elif defined(CONFIG_ESP_CONSOLE_USB_SERIAL_JTAG) && defined(CONFIG_IDF_TARGET_ESP32P4)
-    esp_console_dev_usb_serial_jtag_config_t hw_config = ESP_CONSOLE_DEV_USB_SERIAL_JTAG_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_console_new_repl_usb_serial_jtag(&hw_config, &repl_config, &repl));
-
-#else
-#error Unsupported console type
-#endif
-
-    for (int count = 0; count < sizeof(cmds) / sizeof(esp_console_cmd_t); count++) {
-        ESP_ERROR_CHECK(esp_console_cmd_register(&cmds[count]));
-    }
-
-    ESP_ERROR_CHECK(esp_console_start_repl(repl));
 }
